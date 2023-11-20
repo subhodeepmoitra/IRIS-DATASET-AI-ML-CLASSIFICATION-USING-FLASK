5703cbf7c0dd949801157944ca01b6950c6f57b3
SUBHODEEP MOITRA <64082425+subhodeepmoitra@users.noreply.github.com>
2023-11-20 10:47:48 +0530

    React and Flask connected using CORS

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,37 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 import numpy as np
+from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Load the trained model
 model = load_model('iris_model.h5')
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json(force=True)
-    features = [float(data['sepal_length']), float(data['sepal_width']), float(data['petal_length']), float(data['petal_width'])]
-    input_features = np.array([features])
-    prediction = model.predict(input_features)
-    predicted_class = np.argmax(prediction, axis=1)[0]
-    
-    # Convert the predicted class back to the original label
-    class_labels = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-    predicted_label = class_labels[predicted_class]
-    
-    return jsonify({'prediction': predicted_label})
+    try:
+        data = request.get_json(force=True)
+        print("Received data:", data)
+
+        features = [float(data['sepal_length']), float(data['sepal_width']), float(data['petal_length']), float(data['petal_width'])]
+        input_features = np.array([features])
+        prediction = model.predict(input_features)
+        predicted_class = np.argmax(prediction, axis=1)[0]
+
+        # Convert the predicted class back to the original label
+        class_labels = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+        predicted_label = class_labels[predicted_class]
+
+        response = jsonify({'prediction': predicted_label})
+        response.headers.add('Access-Control-Allow-Origin', '*')  # Allow all origins
+
+        return response
+
+    except Exception as e:
+        print("Error:", e)
+        return jsonify({'error': 'Something went wrong'}), 500  # Internal Server Error
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- frontend/src/Iris.jsx
+++ frontend/src/Iris.jsx
@@ -1,30 +1,49 @@
-// src/App.js
 import React, { useState } from 'react';
-import axios from 'axios';
+import './App.css'; // Import the CSS file for styling
 
 function App() {
   const [sepalLength, setSepalLength] = useState('');
   const [sepalWidth, setSepalWidth] = useState('');
   const [petalLength, setPetalLength] = useState('');
+  const [petalWidth, setPetalWidth] = useState('');
   const [prediction, setPrediction] = useState('');
 
   const handlePredict = async () => {
     // Validate user input
-    if (!sepalLength || !sepalWidth || !petalLength || isNaN(sepalLength) || isNaN(sepalWidth) || isNaN(petalLength)) {
-      alert('Please enter valid input for all three features.');
+    if (
+      !sepalLength ||
+      !sepalWidth ||
+      !petalLength ||
+      !petalWidth ||
+      isNaN(sepalLength) ||
+      isNaN(sepalWidth) ||
+      isNaN(petalLength) ||
+      isNaN(petalWidth)
+    ) {
+      alert('Please enter valid input for all features.');
       return;
     }
 
     try {
-      // Send a POST request to the server
-      const response = await axios.post('http://127.0.0.1:5000/predict', {
-        sepalLength,
-        sepalWidth,
-        petalLength
+      // Send a POST request to the server with JSON data
+      const response = await fetch('http://127.0.0.1:5000/predict', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          sepal_length: sepalLength,
+          sepal_width: sepalWidth,
+          petal_length: petalLength,
+          petal_width: petalWidth
+        })
       });
 
+      // Parse the response as JSON
+      const data = await response.json();
+
       // Update the state with the prediction
-      setPrediction(response.data.prediction);
+      setPrediction(data.prediction);
     } catch (error) {
       console.error('Prediction error:', error);
       alert('Error occurred during prediction. Please try again.');
@@ -32,8 +51,22 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Iris Prediction App</h1>
+    <div className="container">
+       <h1>
+        Iris Prediction App
+        <span className="model-info">
+          Using Neural Network
+        </span>
+      </h1>
+      <p>This app predicts the category of iris based on the provided features.</p>
+      <span className="model-info">
+         Hidden Layers: 2
+        </span>
+        <span className="model-info">
+        Activation Functions used: ReLU, Softmax <br/>
+        &nbsp;
+        </span>
+      {/* ... (your existing input fields, button, and prediction display) */}
       <label>
         Enter Sepal Length:
         <input
@@ -56,6 +89,14 @@ function App() {
           type="text"
           value={petalLength}
           onChange={(e) => setPetalLength(e.target.value)}
+        />
+      </label>
+      <label>
+        Enter Petal Width:
+        <input
+          type="text"
+          value={petalWidth}
+          onChange={(e) => setPetalWidth(e.target.value)}
         />
       </label>
       <button onClick={handlePredict}>Predict</button>
